dcf9179f6e4ed90334ab35873a87d3070c6499b2
Smayan340 <76809078+Smayan340@users.noreply.github.com>
2021-02-06 18:32:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,64 @@
-var cat, mouse;
-var catI, mouseI
-var backgroundI;
+var tom, jerry;
+var catI, catI2, catI3;
+var mouseI, mouseI2, mouseI3;
+var backgroundI, canvas;
+
 function preload() {
-    //load the images here
-    backgroundI = loadImage(garden.png)
-    catI.loadImage(cat3.png)
-    mouseI.loadImage(mouse2.png)
+    //images and animation added here
+    backgroundI = loadImage("images/garden.png");
+    catI = loadAnimation("images/cat1.png");
+    catI2 = loadAnimation("images/cat2.png", "images/cat3.png");
+    catI3 = loadAnimation("images/cat4.png");
+    mouseI = loadAnimation("images/mouse1.png");
+    mouseI2 = loadAnimation("images/mouse2.png", "images/mouse3.png");
+    mouseI3 = loadAnimation("images/mouse4.png");
 
 }
 
 function setup(){
-    createCanvas(1000,800);
-    mouse = createSprite(600,400,50,50);
-    cat = createSprite(100,400,100,100);
+    canvas = createCanvas(800,600);
+
     //create tom and jerry sprites here
+    tom = createSprite(750, 500);
+    tom.addAnimation("tomSleeping", catI);
+    tom.scale = 0.2;
+
+    jerry = createSprite(200, 500);
+    jerry.addAnimation("jerryStanding", mouseI);
+    jerry.scale = 0.15;
 
 }
 
 function draw() {
 
-    background(255);
-    backgroundI.addImage(background)
-    catI.addImage(cat)
-    mouseI.addImage(mouse)
-    if(keyPressed(LEFT_ARROW)){
-        cat.velocityX = -10;
-    }
-    //Write condition here to evalute if tom and jerry collide
+    background(backgroundI);
+    
+   
+    //if tom and jerry collide
+    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
+    { 
+        tom.velocityX=0;
+        tom.addAnimation("tomLastImage", catI3);
+        tom.x =300;
+        tom.scale=0.2;
+        tom.changeAnimation("tomLastImage");
+        jerry.addAnimation("jerryLastImage", mouseI3);
+        jerry.scale=0.15;
+        jerry.changeAnimation("jerryLastImage");
+    }  
 
     drawSprites();
 }
 
+function keyPressed(){
 
-
+    if(keyCode === LEFT_ARROW){
+        tom.velocityX = -5; 
+        tom.addAnimation("tomRunning", catI2);
+        tom.changeAnimation("tomRunning");
+        
+        jerry.addAnimation("jerryTeasing", mouseI2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryTeasing");
+    }
+}
